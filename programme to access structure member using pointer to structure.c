e63afc0ad780eb9433802b331b7984ebefeddad1
Adithya D U <adityadu039@gmail.com>
2025-11-25 11:48:07 +0530

    Add files via upload

---
+++ programme to access structure member using pointer to structure.c
@@ -0,0 +1,13 @@
+//programme to access structure member using pointer to structure
+#include <stdio.h>
+struct student{
+int id;
+char name[20];
+};
+int main() {
+    struct student s={101,"Anu"};
+    struct student*ptr=&s;
+    printf("ID:%d\n",ptr->id);
+    printf("Name:%s\n",ptr->name);
+    return 0;
+}
